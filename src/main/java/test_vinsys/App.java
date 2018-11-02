1e708806d25998de58fec0286cd0c09a4bedb6bf
Mangesh <johndoe@example.com>
2018-11-02 18:32:09 +0530

    First GIT Commit

---
+++ src/main/java/test_vinsys/App.java
@@ -0,0 +1,13 @@
+package test_vinsys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
